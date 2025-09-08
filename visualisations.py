b6314fcec0dfe8efe1738fa9e2fc7f2b69501832
William <william.sarsfield@student.manchester.ac.uk>
2025-09-08 16:38:53 +0100

    Introduce latest data Tweak styling Add figshare link

--- visualisations.py
+++ visualisations.py
@@ -1,9 +1,10 @@
 import plotly.graph_objects as go
 import textwrap
+import pandas as pd
 
 '''Data should only be filtered by indicator and ITL1 regions'''
 
-def gauge(data, region, indicator, selected_year, bounds):
+def gauge(data, region, indicator, selected_year, bounds, fontsize=36):
     data = data.loc[data['year'] == int(selected_year), :]
     median = data[indicator].median()
     temp = data.loc[(data['name'] == region), :]
@@ -13,9 +14,9 @@ def gauge(data, region, indicator, selected_year, bounds):
     fig = go.Figure(go.Indicator(
         mode="gauge+number",
         value=value,
-        title={'text': '<br>'.join(textwrap.wrap(f"{region} {indicator}, {selected_year}", width=50)),
+        title={'text': '<br>'.join(textwrap.wrap(f"{region} {indicator}, {selected_year}", width=40)),
             'font': {'size': 16}},
-        number={'font': {'size': 36},
+        number={'font': {'size': fontsize},
                 'prefix': "£"},  # Adjust the number text size
         gauge={
             'axis': {'range': [bounds[0], bounds[1]]},  # Adjust range as needed
@@ -25,11 +26,12 @@ def gauge(data, region, indicator, selected_year, bounds):
                 {'range': [median * 0.95, median * 1.05], 'color': "#fcbf0b"},
                 {'range': [median * 1.05, data[indicator].max() * 1.2], 'color': "#00979e"}
             ],
-        }
+        },
+        domain={'x': [0.2, 0.8]}  # shrink gauge within figure
     ))
     fig.update_layout(
-        width=400,  # Set the desired width in pixels
-        height=275  # Set the desired height in pixels
+        autosize=True,
+        height=290
     )
     return fig
 
@@ -74,32 +76,45 @@ def time_series(data, regions, uk_data):
             '<br>'.join(textwrap.wrap(f"Time Series for {regions[0]} against {regions[1]} - <b>GVA per hour (chained 2022)</b>", width=100)) +
             '</span>',
             'font': {'size': 14},
+            'x': 0.05,  # move slightly to the right (0=left, 1=right)
+            'y': 0.9,  # move slightly up (1=top of plotting area)
+            'xanchor': 'left',   # align title relative to x
+            'yanchor': 'top',    # align title relative to y
         },
         xaxis_title="Year",
         yaxis_title=f"GVA per hour (chained 2022) (%)",
-        width=800,  # Set the desired width
-        height=400,  # Set the desired height
+        autosize=True,
         template="plotly_white",  # Use a clean white background
         legend=dict(
             orientation="h",  # Horizontal legend
             y=-0.3,  # Position below the chart
-            x=0.5,  # Center the legend horizontally
-            xanchor="center",  # Anchor the legend at the center
+            x=-0.02,  # Center the legend horizontally
+            xanchor="left",  # Anchor the legend
             font=dict(size=16)
-        )
+        ),
+        height=350
     )
     
     return fig
 
-def spider(data, indicators, region, selected_year, colour):
+def spider(data, indicators, region, selected_year, colour, driver):
     # Filter the data for the selected region and year
-    data = data.loc[data['year'] == int(selected_year), :]
+    # data = data.loc[data['year'] == int(selected_year), :]
+    # Build the output DataFrame
+    row = pd.DataFrame({'name': data['name'].unique()})
+    for indicator, years in driver.items():
+        # Get the year for this period
+        year_val = int(years[1])
+        # Subset data for this year and column
+        sub = data.loc[data['year'] == year_val, ['name', indicator]]
+        # Merge so values align by code
+        row = row.merge(sub, on='name', how='left')
     
     # Compute the medians for each column in the indicators
-    medians = data.drop(columns='name').median()[indicators]
+    medians = row.drop(columns='name').median()[indicators]
     
     # Get the relative values for each indicator
-    temp = data.loc[data['name'] == region, indicators]
+    temp = row.loc[row['name'] == region, indicators]
     temp = (temp / medians) * 100
 
     # Handle missing data by filtering out NaN values
@@ -110,8 +125,8 @@ def spider(data, indicators, region, selected_year, colour):
     r_values = temp.values.flatten().tolist()
     r_values.append(r_values[0])  # Append the first value to close the loop
 
-    theta_values = valid_indicators
-    theta_values.append(theta_values[0])  # Append the first category to close the loop
+    theta_values = ['<br>'.join(textwrap.wrap(ind, width=10)) for ind in valid_indicators]
+    theta_values.append(theta_values[0])  # Close the loop
 
     # Create a time series plot
     fig = go.Figure()
@@ -142,20 +157,23 @@ def spider(data, indicators, region, selected_year, colour):
             radialaxis=dict(
                 visible=True,
                 range=[20, 200],  # Adjust the range as needed
-                showticklabels=False  # Remove radial axis ticks
-
-            )
+                showticklabels=False,  # Remove radial axis ticks
+                tickfont=dict(size=8)
+            ),
+            angularaxis=dict(
+                tickfont=dict(size=12),
+            ),
+            domain=dict(x=[0.2, 0.8]),  # shrink spider within figure
         ),
         title={
             'text': '<span style="font-weight:normal;">' + 
-            '<br>'.join(textwrap.wrap(f"Spider Plot of <b>Productivity Indicators</b> {region} - {selected_year}",width=70)) +
+            '<br>'.join(textwrap.wrap(f"Spider Plot of <b>Productivity Indicators</b> {region} - <i>latest available data",width=60)) +
             '</span>',
             'font': {'size': 14},
         },
-        width=400,  # Set the desired width in pixels
-        height=400,  # Set the desired height in pixels
+        autosize=True,
         template="plotly_white",  # Use a clean white background
-        showlegend=False
+        showlegend=False,
     )
     
     return fig
@@ -193,10 +211,10 @@ def bar(data, indicator, regions, driver):
         },
         xaxis_title="Year",
         yaxis_title=f"{indicator} ({unit})",
-        width=750,  # Set the desired width
-        height=350,  # Set the desired height
+        autosize=True,
         template="plotly_white",  # Use a clean white background
-        showlegend=False
+        showlegend=False,
+        height=450
     )
     
     return fig
